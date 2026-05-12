2d0df6d410bafe69f0b901fe9d0c3d521a4b6a9c
Victor Santos <menezes.victorsantos@outlook.com>
2026-05-12 18:34:32 -0300

    feat: login and shop module

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { apiUrl } from "../api";
 import { Product } from "./types";
+import { Observable } from "rxjs";
 
 
 
@@ -11,12 +12,20 @@ import { Product } from "./types";
 export class ProductsService {
     constructor(private readonly httpClient: HttpClient) { }
 
-    getAllProducts() {
+    getProducts(): Observable<Product[]> {
         return this.httpClient.get<Product[]>(apiUrl)
     }
 
-    editProductBySKU(SKU: string, product: Product) {
+    editProductBySKU(SKU: string, product: Product): Observable<Product | undefined> {
         return this.httpClient.put<Product>(`${apiUrl}/${SKU}`, product)
+    }
+
+    deleteProductBySKU(SKU: string): Observable<Product | undefined> {
+        return this.httpClient.delete<Product>(`${apiUrl}/${SKU}`)
+    }
+
+    createProduct(product: Product): Observable<Product> {
+        return this.httpClient.post<Product>(`${apiUrl}`, product)
     }
 
 }
